4345e7b51c9c9a1fa7d61f430c7ba11722475ff8
Ipheee <132576780+Ipheee@users.noreply.github.com>
2024-06-04 10:50:43 +0100

    Optimized page layout for e-commerce site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,19 @@
-// import React from "react";
-// import Styles from "./Header.css";
+import React from "react"
 import "./Header.css"
 
 export const Header = () => {
   return (
     <section>
-        {/* <header className={Styles.headerOne}> */}
         <header className="headerOne">
-            {/* <section className={Styles.sectionOne}> */}
             <section className="sectionOne">
                 <p><img src="/src/assets/telephone-icon.png" alt=""/> (225) 555-0118</p>
                 <p><img src="/src/assets/mail-icon.png" alt=""/> michelle.rivera@example.com</p>
             </section>
-            {/* <section className={Styles.sectionTwo}> */}
             <section className="sectionTwo">
                 <p>Follow Us and get a chance to win 80% off</p>
             </section>
-            {/* <section className={Styles.sectionThree}> */}
             <section className="sectionThree">
                 <p>Follow Us :</p>
-                {/* <div className={Styles.socialMediaIcons}> */}
                 <div className="socialMediaIcons">
                     <img src="/src/assets/instagram-icon.png" alt="" />
                     <img src="/src/assets/youtube-icon.png" alt="" />
@@ -45,12 +39,6 @@ export const Header = () => {
                 </ul>
             </nav>
             <section className="sectionFour">
-                {/* <p><img src="/src/assets/profile-icon.png" alt="" /> Login / Register</p>   
-                <div className="siteIcons">
-                    <img src="/src/assets/search-icon.png" alt="" />
-                    <img src="/src/assets/cart-icon.png" alt="" />
-                    <img src="/src/assets/like-icon.png" alt="" />
-                </div> */}
                 <a href="#" className="sectionFourLink">Login / Register</a>
                 <a href="#" className="siteIcons"><img src="/src/assets/search-icon.png" alt="" /></a>
                 <a href="#" className="siteIcons"><img src="/src/assets/cart-icon.png" alt="" /></a>
@@ -75,7 +63,6 @@ export const Header = () => {
                             <h2>FURNITURE</h2>
                             <a>Read More</a>
                         </div>
-
                     </div>
                     <div className=" card cardThree">
                         <img src="/src/assets/cardThree.png" alt=""/>
@@ -84,7 +71,6 @@ export const Header = () => {
                             <h2>FURNITURE</h2>
                             <a>Read More</a>
                         </div>
-
                     </div>
                     <div className="card cardFour">
                         <img src="/src/assets/cardFour.png" alt=""/>
@@ -96,9 +82,6 @@ export const Header = () => {
                     </div>
                 </div>
             </section>
-
-
-
             <section className="sectionSix">
                 <h2>Featured Products</h2>
                 <h1>BESTSELLER PRODUCTS</h1>
@@ -144,28 +127,24 @@ export const Header = () => {
                         <p>English Department</p>
                         <p className="price"><span className="originalPrice">$16.48</span><span className="discountedPrice"> $6.48</span></p>
                     </div>                    
-               
                     <div className="productItems">
                         <img src="/src/assets/cardEleven.png" alt="" />
                         <h3>Graphic Design</h3>
                         <p>English Department</p>
                         <p className="price"><span className="originalPrice">$16.48</span><span className="discountedPrice"> $6.48</span></p>
                     </div>                    
-               
                     <div className="productItems">
                         <img src="/src/assets/cardEight.png" alt="" />
                         <h3>Graphic Design</h3>
                         <p>English Department</p>
                         <p className="price"><span className="originalPrice">$16.48</span><span className="discountedPrice"> $6.48</span></p>
                     </div>                    
-                
                     <div className="productItems">
                         <img src="/src/assets/cardTweleve.png" alt="" />
                         <h3>Graphic Design</h3>
                         <p>English Department</p>
                         <p className="price"><span className="originalPrice">$16.48</span><span className="discountedPrice"> $6.48</span></p>
                     </div>                   
-                
                     <div className="productItems">
                         <img src="/src/assets/cardThirteen.png" alt="" />
                         <h3>Graphic Design</h3>
@@ -173,7 +152,6 @@ export const Header = () => {
                         <p className="price"><span className="originalPrice">$16.48</span><span className="discountedPrice"> $6.48</span></p>
                     </div>                    
                 </div>
-                 
             </section>
             <section className="sectionSix">
                 <h2>Featured Products</h2>
@@ -192,18 +170,15 @@ export const Header = () => {
                         <h2>Easy Wins</h2>
                         <p>Get your best looking smile now!</p>
                     </div>    
-
                     <div className="column">
                         <img src="/src/assets/concrete.png" alt="" />
                         <h2>Concrete</h2>
                         <p>Defalcate is most focused in helping you discover your most beautiful smile</p>
                     </div>
-
                     <div className="column">
                         <img src="/src/assets/hackGrowth.png" alt="" />
                         <h2>Hack Growth</h2>
                         <p>Overcame any hurdle or any other problem.</p>
-
                     </div>
                 </div>
             </section>
